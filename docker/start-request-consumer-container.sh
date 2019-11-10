9b054a29c8380b002ee30d44972ddb79ab8c6ce2
Param Singh <param@metabrainz.org>
2019-11-10 13:35:15 +0100

    Fix misc bugs in the request consumer stuff
    And instead of making a main function, call it via manage.py

--- docker/start-request-consumer-container.sh
+++ docker/start-request-consumer-container.sh
@@ -24,4 +24,4 @@ docker run \
         --conf "spark.executor.memory"=$EXECUTOR_MEMORY \
         --conf "spark.driver.memory"=$DRIVER_MEMORY \
         --py-files listenbrainz_spark_request_consumer.zip \
-        listenbrainz_spark/request_consumer/main.py
+	manage.py request_consumer
