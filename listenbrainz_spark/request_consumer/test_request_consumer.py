7130aac2a3c43abbb949d1d54948c0948d555ed9
Param Singh <iliekcomputers@gmail.com>
2019-12-04 18:56:08 +0000

    Add first test for request consumer

---
+++ listenbrainz_spark/request_consumer/test_request_consumer.py
@@ -0,0 +1,21 @@
+from unittest.mock import patch, MagicMock
+
+from listenbrainz_spark.tests import SparkTestCase
+from listenbrainz_spark.request_consumer.request_consumer import RequestConsumer
+
+
+class RequestConsumerTestCase(SparkTestCase):
+
+    def setUp():
+        self.consumer = RequestConsumer()
+        self.consumer.rabbitmq = MagicMock()
+        self.consumer.request_channel = MagicMock()
+        self.consumer.result_channel = MagicMock()
+
+    @patch('listenbrainz_spark.request_consumer.request_consumer.init_rabbitmq')
+    def test_connect_to_rabbitmq(self, mock_init_rabbitmq):
+        with create_app().app_context():
+            self.consumer.connect_to_rabbitmq()
+            mock_init_rabbitmq.assert_called_once()
+
+
